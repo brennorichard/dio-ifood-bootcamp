3dc3910a647d9927466794e34500c6d61bad902d
brennorichard <brennotrich2002@gmail.com>
2022-11-03 20:01:42 -0300

    Adicionando testes de variaveis e operadores

---
+++ TIPOS-VARIAVEIS/src/Operadores.java
@@ -0,0 +1,58 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("uma das condições é verdadeira");
+        }
+
+        // String nome1 = "Brenno";
+        // String nome2 = new String("Brenno");
+
+        // System.out.println(nome1.equals(nome2));
+
+        // int numero1 = 1;
+        // int numero2 = 2;
+
+        // boolean simNao = numero1 == numero2;
+
+        // if (numero1 < numero2) {
+        // System.out.println("a nossa condição é verdadeira");
+        // }
+
+        // System.out.println("numero1 é igual a numero2?" + simNao);
+
+        // simNao = numero1 != numero2;
+
+        // System.out.println("numero1 é diferente de numero2" + simNao);
+
+        // int a, b;
+
+        // a = 5;
+        // b = 6;
+
+        // String resultado = a == b ? "verdadeiro" : "falso";
+
+        // boolean variavel = true;
+
+        // int numero = 5;
+
+        // System.out.println(numero);
+
+        // numero = numero * -1;
+
+        // System.out.println(numero);
+
+        // numero++;
+
+        // numero--;
+
+    }
+
+}
---
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+    }
+}
